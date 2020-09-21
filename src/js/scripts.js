0cb76665f3462e4e88179dc36c22bff66f69381d
ramsua <richardramsua@gmail.com>
2020-09-20 21:04:29 -0300

    async await running

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,17 @@
-import { f } from "./modules/module";
-f()
+const { default: callAsyncFunction } = require('./modules/module');
+
+const toggleTheme = document.getElementById('toggle-theme')
+const body = document.getElementById('theme');
+
+toggleTheme.addEventListener('click', () => {
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme')
+        body.classList.add('dark-theme')
+        toggleTheme.textContent = 'Light theme'
+    } else {
+        body.classList.remove('dark-theme')
+        body.classList.add('light-theme')
+        toggleTheme.textContent = 'Dark theme'
+    }
+    callAsyncFunction()
+})
